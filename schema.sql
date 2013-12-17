e293767b7aeb75e803d15ebf30681a6958e3e0eb
Kate <katepdonahue@gmail.com>
2013-12-16 20:12:54 -0500

    Add data to tables

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE projects
 (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, title TEXT, category TEXT, CHECK (category IN ("Music", "Books", "Charity")), funding_goal INTEGER, start_date DATE, end_date DATE);
 CREATE TABLE users
 (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, name TEXT, age INTEGER);
-CREATE TABLE pledge
+CREATE TABLE pledges
 (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, amount INTEGER, user_id INTEGER, project_id INTEGER);
