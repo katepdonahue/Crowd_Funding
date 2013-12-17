2159917ed0ba8517726dc43083d791ec385a6f1d
Kate <katepdonahue@gmail.com>
2013-12-16 19:35:17 -0500

    Create schema, fix constraint later

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE projects
+(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, title TEXT, CHECK (category IN ("Music", "Books", "Charity")), funding_goal INTEGER, start_date DATE, end_date DATE);
+CREATE TABLE users
+(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, name TEXT, age INTEGER);
+CREATE TABLE pledge
+(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, amount INTEGER, user_id INTEGER, project_id INTEGER);
